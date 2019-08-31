09de8bdcff6469ce2f03fa81444426842dca44ff
keerthivasan123 <keerthivasan287@gmail.com>
2019-08-31 22:41:36 +0530

    Models for inventroy management

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,33 @@
+from django.contrib import admin
+# Register your models here.
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+
+    type = models.CharField(max_length=200, blank=False)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    choices = (
+        ('AVAILABLE', 'Item ready to be purchased'),
+        ('SOLD', 'Item already purchased'),
+        ('RESTOCKING', 'Item restocking in few days')
+    )
+
+    status = models.CharField(max_length=10, choices=choices, default='SOLD')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return 'Type: {0} Price: {1}'.format(self.type, self.price)
+
+class Desktops(Device):
+    pass
+
+class Laptops(Device):
+    pass
+
+class Mobiles(Device):
+    pass
